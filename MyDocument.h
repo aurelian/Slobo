89e3ccc608332d4522eced6c62e80824ae6e3dd0
aurelian <oancea@gmail.com>
2009-11-12 21:30:37 +0100

    first commit

---
+++ MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  Slobo
+//
+//  Created by Aurelian Oancea on 11/12/09.
+//  Copyright __MyCompanyName__ 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
---
+++ MyDocument.m
@@ -0,0 +1,33 @@
+//
+//  MyDocument.m
+//  Slobo
+//
+//  Created by Aurelian Oancea on 11/12/09.
+//  Copyright __MyCompanyName__ 2009 . All rights reserved.
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init 
+{
+    self = [super init];
+    if (self != nil) {
+        // initialization code
+    }
+    return self;
+}
+
+- (NSString *)windowNibName 
+{
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)windowController 
+{
+    [super windowControllerDidLoadNib:windowController];
+    // user interface preparation code
+}
+
+@end
